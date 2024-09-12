46461d82430c6a6cb0cec02f9e95beb2e3dab9c7
Your Name <you@example.com>
2024-09-12 21:41:59 +0000

    Remove extra spaces and end

--- star_box_logic.py
+++ star_box_logic.py
@@ -1,12 +1,9 @@
 def generate_star_box(width, height):
     if width < 1 or height < 1:
         return []
-    
-    if height == 1:
-        return ['*' * width]
-    
+
     star_box = []
     for _ in range(height):
         star_box.append('*' * width)
-    
+
     return star_box
